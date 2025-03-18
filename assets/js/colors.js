ca4e621bd90fa343cafc62d76a8a41335d9973ad
Alexis BENOIT <alex@noesya.coop>
2025-03-18 18:35:20 +0100

    color mode

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,7 +1,6 @@
 window.gaite = window.gaite || {};
 
-gaite.colors = [
-  "#FFFFFF",
+window.gaite.colors = [
   "#F7AAFF",
   "#F6FD05",
   "#F7B20F",
@@ -9,14 +8,39 @@ gaite.colors = [
   "#79FE42"
 ];
 
-gaite.init = function () {
-  this.setRandomColor();
+window.gaite.isColorMode = false;
+
+window.gaite.init = function () {
+  this.button = document.querySelector('.change-color-mode-button');
+
+  if (this.button) {
+    this.button.addEventListener('click', this.toggleMode.bind(this));
+  }
 };
 
-gaite.setRandomColor = function () {
+window.gaite.toggleMode = function () {
+  var color = "#FFFFFF";
+
+  this.button.classList.remove('is-color-mode');
+
+  this.isColorMode = !this.isColorMode;
+
+  if (this.isColorMode) {
+    color = this.getRandomColor();
+    this.button.classList.add('is-color-mode');
+  }
+
+  this.setColor(color);
+};
+
+window.gaite.getRandomColor = function () {
   var index = Math.floor(Math.random() * this.colors.length),
     color = this.colors[index];
+  return color;
+};
+
+window.gaite.setColor = function (color) {
   document.documentElement.style.setProperty("--color-background", color);
 };
 
-gaite.init();
+window.gaite.init();
